63ec3af6c7cb3d74a07336b43e23d35378ab0946
Ariel Segura <ariel.segura@yahoo.com>
2018-08-02 15:54:12 -0300

    remove unused components

--- alexander-integration-tests/src/test/java/com/mulesoft/mozart/recommender/tests/IntegrationTest.java
+++ alexander-integration-tests/src/test/java/com/mulesoft/mozart/recommender/tests/IntegrationTest.java
@@ -3,11 +3,9 @@ package com.mulesoft.mozart.recommender.tests;
 import com.mulesoft.mozart.recommender.learner.client.AlexanderLearnerAPI;
 import com.mulesoft.mozart.recommender.mocks.MappingSuggestionRequestMocks;
 import com.mulesoft.mozart.recommender.rest.FitResponse;
-import com.mulesoft.mozart.tests.platform.utils.TestConfigurationProperties;
 import com.mulesoft.recommenderapiservice.rest.MappingSuggestionRequest;
 import com.mulesoft.recommenderapiservice.rest.MappingSuggestionResponse;
 import com.mulesoft.recommenderapiservice.restclient.RecommenderAPIServiceClient;
-import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,21 +18,10 @@ public class IntegrationTest {
 
     static Logger logger = LoggerFactory.getLogger(IntegrationTest.class);
 
-    private final String organizationId = AlexanderEnvironmentConfiguration.getConfiguration().getProperty("anypoint.organizationId");
     AlexanderLearnerAPI alexanderLearnerAPI = AlexanderLearnerAPI
             .connect(AlexanderEnvironmentConfiguration.getConfiguration().getProperty("alexander-learner-url"), 2000, 40000);
     RecommenderAPIServiceClient recommenderAPIServiceClient = RecommenderAPIServiceClient
             .connect(AlexanderEnvironmentConfiguration.getConfiguration().getProperty("recommender-api-url"), 2000, 40000);
-
-    @Before
-    public void setUp() throws Exception {
-        TestConfigurationProperties testConfig = new TestConfigurationProperties();
-        testConfig.setBaseUrl(AlexanderEnvironmentConfiguration.getConfiguration().getProperty("anypoint.baseurl"));
-        testConfig.setUser(AlexanderEnvironmentConfiguration.getConfiguration().getProperty("anypoint.username"));
-        testConfig.setPassword(AlexanderEnvironmentConfiguration.getConfiguration().getProperty("anypoint.password"));
-        testConfig.setDefaultTimeout(15000);
-        testConfig.setOrganizationId(organizationId);
-    }
 
     @Test(timeout = 40000)
     public void canStoreInitialSuggestion() throws Exception {
